11d5e3ca3ca81017088b5889dfabe40943e16074
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-18 11:30:25 +0300

    add dotenv

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import dotenv from "dotenv";
 import jokeApi from "./src/api/joke.api";
+dotenv.config();
 
 const app = express();
 
@@ -8,6 +10,6 @@ app.use(express.json());
 
 app.use(jokeApi);
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server is running on port 8000");
 });
