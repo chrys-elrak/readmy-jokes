eb7913350cec2b560061236456cabadb671db009
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-20 23:07:34 +0300

    Hello world end point

--- server.ts
+++ server.ts
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1', joke);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 const PORT = process.env.PORT || 3000;
 
 
