4149c2d1bd08f62014d8588d666d42005c717394
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-23 07:53:15 +0300

    fix issue

--- src/utils/getJoke.ts
+++ src/utils/getJoke.ts
@@ -1,3 +1,4 @@
+import joke from '../joke';
 import { Lang } from '../types/lang';
 import { Joke, JokeType } from './../types/joke';
 export default async function getJoke(jokeType: JokeType, lang: Lang = Lang.DEFAULT): Promise<Joke<JokeType>> {
@@ -6,25 +7,26 @@ export default async function getJoke(jokeType: JokeType, lang: Lang = Lang.DEFA
         case Lang.EN: {
             let { default: en } = await import('../../assets/jokes/jokes.en.json');
             db = { ...db, ...en };
+            break;
         }
         case Lang.MG: {
             let { default: mg } = await import('../../assets/jokes/jokes.mg.json');
             db = { ...db, ...mg };
+            break;
         } case Lang.FR: {
             let { default: fr } = await import('../../assets/jokes/jokes.fr.json');
             db = { ...db, ...fr };
+            break;
         }
         default: {
             let { default: def } = await import('../../assets/jokes/jokes.default.json');
             db = { ...db, ...def };
+            break;
         }
     }
     // turn db into array
     const jokes = Object.keys(db).map((key: string) => db[key]).filter((joke: Joke<JokeType>) => joke.type === jokeType);
     // get random joke
     const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
-    if (!randomJoke) {
-        throw new Error('No joke found');
-    }
     return randomJoke;
 }
