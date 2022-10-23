011a6b00c6d3c09d1eff96a7d4aa4a71fc674eea
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-23 07:54:02 +0300

    return error

--- src/joke.ts
+++ src/joke.ts
@@ -1,13 +1,15 @@
+import { Lang } from './types/lang';
 import { Request, Response } from 'express';
 import { renderJoke, renderMemes, renderQa } from '../src/renderer';
 import { Joke, JokeType } from '../src/types/joke';
 import getJoke from '../src/utils/getJoke';
 
-export default function (req: Request, res: Response) {
+export default async function (req: Request, res: Response) {
     try {
         let svg: string = '';
         const jokeType = (req.query as any)?.jokeType?.toUpperCase();
-        const joke = getJoke(jokeType as JokeType);
+        const lang: Lang = (req.query as any)?.lang?.toUpperCase();
+        const joke = await getJoke(jokeType as JokeType, lang);
         if (joke.type === "QA") {
             svg = renderQa(joke as Joke<"QA">);
         }
@@ -21,6 +23,16 @@ export default function (req: Request, res: Response) {
         res.setHeader('Cache-Control', 's-maxage=86400');
         return res.send(svg);
     } catch {
-        res.status(500).send('An error was encountered while processing your request');
+        res.header('Content-Type', 'text/html');
+        res.status(500).send(`
+            <p class="error">
+                Uh oh! An error was encountered while processing your request.
+            </p>
+            <style>
+                .error {
+                    color: red;
+                }
+            </style>
+        `);
     }
 }
